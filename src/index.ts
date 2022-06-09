b8e30d3e2737542c32e0bb21905b95c9b4368ed7
Andrey Makhnev <headlulu888@gmail.com>
2022-06-09 13:30:14 +0300

    change port variable

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express";
 import cors from "cors"
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 // app.use(bodyParser.json({ type: 'application/*+json' }))
